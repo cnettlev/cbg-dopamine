fd083fefb2927f3b6b472e20e5d8a26b81f1816d
cnettlev <cristobal.nettle@gmail.com>
2020-04-08 11:45:15 -0400

    Added experiment for garivier and molines 2008

---
+++ garivier-molines-2008/exp.sh
@@ -0,0 +1,7 @@
+
+
+python learning.py -d4.0 -f001 -N 0.01 -l6.5e-06 -L9e-05 -g -t 200 -b 12 --ltd-constant --correlatedNoise -S --relativeValue -F/mnt/Data/Cristobal/tonicDA_effects/dynamicDA/20200408_garivier --debug -P &
+python learning.py -d5.0 -f001 -N 0.01 -l6.5e-06 -L9e-05 -g -t 200 -b 12 --ltd-constant --correlatedNoise -S --relativeValue -F/mnt/Data/Cristobal/tonicDA_effects/dynamicDA/20200408_garivier --debug -P &
+python learning.py -d6.0 -f001 -N 0.01 -l6.5e-06 -L9e-05 -g -t 200 -b 12 --ltd-constant --correlatedNoise -S --relativeValue -F/mnt/Data/Cristobal/tonicDA_effects/dynamicDA/20200408_garivier --debug -P &
+
+# python learning.py -d8.0 -f001 -N 0.01 -l6.5e-06 -L9e-05 -g -t 200 -b 12 --ltd-constant --correlatedNoise -S --relativeValue -F/mnt/Data/Cristobal/tonicDA_effects/dynamicDA/20200408_garivier --debug -P &
